07783160ee7c116d34138f7537a4c42f848278ac
Matheus <mrogeriopesarini@gmail.com>
2025-01-20 00:08:44 -0300

    Refatora rotas de usuários: move para novo módulo, adiciona suporte a CORS e Helmet

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,16 +1,22 @@
 import Fastify from "fastify";
-import { userRoutes } from "./users/user";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+
+import { userRoutes } from "./routes/get-user/user";
 
 const fastify = Fastify({ logger: true });
+
+fastify.register(helmet);
+fastify.register(cors, {origin: true});
 
 fastify.register(userRoutes);
 
 const start = async () => {
 	try {
 		await fastify.listen({ port: 3000 });
-		fastify.log.info(
-			`Servidor rodando no endereco: ${fastify.server.address()}`,
-		);
+		const address = fastify.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+    fastify.log.info(`Server listening on port ${port}`);
 	} catch (error) {
 		fastify.log.error(error);
 		process.exit(1);
