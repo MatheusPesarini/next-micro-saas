738c746e54c37c26e6efc6b87934161894834c83
Matheus <mrogeriopesarini@gmail.com>
2025-01-21 21:57:20 -0300

    Melhora a interface dos formulários de login e registro, adicionando classes de estilo e centralizando os elementos

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,9 +23,19 @@ export default function Login() {
 	return (
 		<div>
 			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<input type="email" placeholder="Digite seu e-mail" name="email" className="text-black"/>
-				<input type="password" placeholder="Digite sua senha" name="password" />
+			<form onSubmit={handleSubmit} className="flex flex-col items-center">
+				<input
+					type="email"
+					placeholder="Digite seu e-mail"
+					name="email"
+					className="text-black"
+				/>
+				<input
+					type="password"
+					placeholder="Digite sua senha"
+					name="password"
+					className="text-black"
+				/>
 				<button type="submit">Logar</button>
 			</form>
 		</div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -23,10 +23,25 @@ export default function Register() {
 	return (
 		<div>
 			<h1>Register</h1>
-			<form onSubmit={handleSubmit}>
-				<input type="text" placeholder="Digite seu nome" name="name" />
-				<input type="email" placeholder="Digite seu e-mail" name="email" />
-				<input type="password" placeholder="Digite sua senha" name="password" />
+			<form onSubmit={handleSubmit} className="flex flex-col items-center">
+				<input
+					type="text"
+					placeholder="Digite seu nome"
+					name="name"
+					className="text-black"
+				/>
+				<input
+					type="email"
+					placeholder="Digite seu e-mail"
+					name="email"
+					className="text-black"
+				/>
+				<input
+					type="password"
+					placeholder="Digite sua senha"
+					name="password"
+					className="text-black"
+				/>
 				<button type="submit">Cadastrar</button>
 			</form>
 		</div>
